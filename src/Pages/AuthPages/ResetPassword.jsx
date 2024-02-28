1bcb748a4b2886c977f7aae49f9afddf3dc6a5ea
Adegoke Gabriel Adewale <111113715+TheRealAdegoke@users.noreply.github.com>
2024-02-28 19:17:00 +0100

    All auth pages are have been connected to the backend

--- src/Pages/AuthPages/ResetPassword.jsx
+++ src/Pages/AuthPages/ResetPassword.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import NavLogo from "../../assets/NavLogo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { ImSpinner6 } from "react-icons/im";
+import axios from "axios";
+import { message } from "antd";
 
 const ResetPassword = () => {
+  const [password, setPassword] = useState("")
+  const [loading, setLoading] = useState(false)
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const token = searchParams.get("token");
+  const navigate = useNavigate()
+
+  const handleCreatePassword = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const postData = {
+      token: token,
+      password: password,
+    };
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/auth/resetpassword", postData
+      );
+      console.log(response.data);
+      message.success(response.data.message)
+      navigate("/login")
+    } catch (error) {
+      console.error(error.response.data.error);
+      message.error(error.response.data.error);
+    } finally {
+      setLoading(false)
+    }
+  }
   return (
     <>
       <main className="bg-[rgb(3,11,21)] min-h-screen text-white">
@@ -14,8 +45,8 @@ const ResetPassword = () => {
           />
         </div>
 
-        <div className="bg-[rgb(2,8,16)] w-4/5 PC:max-w-[400px] w mx-auto pb-8 border border-[rgb(64,65,67)] rounded-[8px]">
-          <form className="px-6">
+        <div className="bg-[rgb(2,8,16)] w-4/5 max-w-[400px] mx-auto pb-8 border border-[rgb(64,65,67)] rounded-[8px]">
+          <form className="px-6" onSubmit={handleCreatePassword}>
             <div className="mt-4 mb-6">
               <h1 className="text-[rgb(201,209,217)] text-2xl font-bold">
                 Reset Password
@@ -37,6 +68,9 @@ const ResetPassword = () => {
                   type="password"
                   className="bg-[rgb(42,42,47)] border border-[rgb(64,65,67)] rounded-[5px] h-[40px] px-3 focus:border-[rgb(54,116,220)] outline-none"
                   placeholder="123456789"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
                 />
               </div>
 
@@ -59,7 +93,13 @@ const ResetPassword = () => {
               type="submit"
               className="text-black bg-white hover:bg-[rgba(255,255,255,0.9)] block mx-auto p-3 rounded-[5px] font-medium w-full"
             >
-              Create Password
+              {loading ? (
+                <div>
+                  <ImSpinner6 className="animate-spin text-2xl text-black block mx-auto" />
+                </div>
+              ) : (
+                "Create Password"
+              )}
             </button>
           </form>
         </div>
